66b5642811dfc2b33df12983a8c23030ad1d0b02
Suyash Nayak <suyashnayak11@gmail.com>
2026-02-05 15:05:45 +0530

    Add files via upload

---
+++ sql_queries/ecommerce_shipping Table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE ecommerce_shipping (
+    ID INT,
+    Warehouse_block VARCHAR(10),
+    Mode_of_Shipment VARCHAR(20),
+    Customer_care_calls INT,
+    Customer_rating INT,
+    Cost_of_the_Product INT,
+    Prior_purchases INT,
+    Product_importance VARCHAR(20),
+    Gender VARCHAR(10),
+    Discount_offered INT,
+    Weight_in_gms INT,
+    Reached_on_Time_Y_N INT
+);
