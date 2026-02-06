516a8b5f57ede8f39cccc39638cb1fb6d9983d45
Suyash Nayak <suyashnayak11@gmail.com>
2026-02-06 17:43:21 +0530

    Update importance_delivery_percentage.sql

--- sql_queries/importance_delivery_percentage.sql
+++ sql_queries/importance_delivery_percentage.sql
@@ -1,3 +1,5 @@
+
+-- Product Importance vs Late Delivery vs On Time & %
 select
 Product_importance,
 case when Reached_on_Time_Y_N = 1 then 'On Time' else 'Late' end as delivery_status,
@@ -5,3 +7,14 @@ count(*) as total_orders,
 round( count(*)*100/ sum( count(*)) over (partition by product_importance), 2 ) as order_percentage
 from ecommerce_shipping
 group by product_importance, Reached_on_Time_Y_N;
+
+
+-- Product Importance vs Late Delivery & %
+select
+Product_importance, 
+count(*) as total_orders,
+sum(case when Reached_on_Time_Y_N = 0 then 1 else 0 end) as late_orders,
+round (sum(case when Reached_on_Time_Y_N = 0 then 1 else 0 end)*100/count(*), 2) as late_percentage
+from ecommerce_shipping
+group by product_importance
+order by late_percentage desc;
